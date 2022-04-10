85f6aa8d7ed4bca63c18335f4e8330cfb0e174ab
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-10 18:25:56 +0530

    Added: Checking equality of two Lines

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -3,35 +3,34 @@ package com.bridgelabz;
 import java.util.Scanner;		// Needed to use the Scanner
 
 public class LineComparision {
-	static Scanner scan = new Scanner(System.in);
-	// Declare variables
-	int x1;
-	int y1;
-	int x2;
-	int y2;
-	
-	public void enterCoordinatesValues(){
-
-		System.out.println("Enter the Coordinates");	// Show prompting message
-		x1 = scan.nextInt();							// Use nextInt() to read an int
-		System.out.println("Enter the Coordinates");
-		y1 = scan.nextInt();
-		System.out.println("Enter the Coordinates");
-		x2 = scan.nextInt();
-		System.out.println("Enter the Coordinates");
-		y2 = scan.nextInt();	
-		System.out.print("Length of Line: " + calculateLength());
-	}
-	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub	
-		System.out.println("Welcome to line comparision");			// Show prompting message
-		LineComparision lineComparison1 = new LineComparision();
-		lineComparison1.enterCoordinatesValues();
-	}
-	public double calculateLength(){
-		//Computation
-		double length = Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2)); 
-			return length;
+		// TODO Auto-generated method stub
+		System.out.println("Welcome to Line Comparison Computation");		// Show prompting message
+		Scanner scan = new Scanner(System.in);								// Use nextInt() to read an int
+		System.out.println("Enter Coordinates of line1 x1,y1: ");
+		int x1 = scan.nextInt();
+		int y1 = scan.nextInt();
+		System.out.println("Enter Coordinates of line1 x2,y2: ");
+		int x2 = scan.nextInt();
+		int y2 = scan.nextInt();
+
+		int line1 = (int) Math.sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+		System.out.println("Line 1 length: " + line1);
+
+		System.out.println("Enter Coordinates of line2 x3,y3: "); // enter value
+		int x3 = scan.nextInt();
+		int y3 = scan.nextInt();
+		System.out.println("Enter Coordinates of line2 x4,y4: ");
+		int x4 = scan.nextInt();
+		int y4 = scan.nextInt();
+
+		int line2 = (int) Math.sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+		System.out.println("Line 2 length: " + line2);
+
+		if (line1 == line2) {
+			System.out.println("Line1 is equal to line2");
+		} else {
+			System.out.println("Line1 is not equal to line2");
+		}
 	}
 }
