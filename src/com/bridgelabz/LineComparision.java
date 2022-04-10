9d347da0ccee728db6b302095f3ca8b07b02c61e
nikhilvdeshmukh <deshmukhnikhil81@gmail.com>
2022-04-10 20:41:14 +0530

    Added: Comparing length of two lines by Object Oriented Programming

--- src/com/bridgelabz/LineComparision.java
+++ src/com/bridgelabz/LineComparision.java
@@ -1,14 +1,14 @@
 package com.bridgelabz;
 //@author: Nikhil
 import java.util.Scanner;			// Needed to use the Scanner
-
-public class LineComparision {	
+public class LineComparision {
 	static Scanner scan = new Scanner(System.in);
 	// Declare variables
-	int x1, y1, x2, y2; 	
+	int x1, y1, x2, y2; 
+	
 	public void enterCoordinates() {
 		System.out.println("Enter coordinate x1");		// Show prompting message
-		x1 = scan.nextInt();							// Use nextInt() to read an int
+		x1 = scan.nextInt();							// Use nextInt() to read an int		
 		System.out.println("Enter coordinate y1");
 		y1 = scan.nextInt();
 		System.out.println("Enter coordinate x2");
@@ -17,8 +17,7 @@ public class LineComparision {
 		y2 = scan.nextInt();
 	}	
 	//compare to method
-	public void compareTwoLengths(Double length1, Double length2) {		
-		// if else condition
+	public static void compareTwoLengths(Double length1, Double length2) {
 		if(length1.compareTo(length2) == 0) {
 			System.out.println("Two lines are equal");
 		}
@@ -29,23 +28,27 @@ public class LineComparision {
 			System.out.println("Line1 is greater than line2");
 		}
 	}
+	// main method
     public static void main(String[] args) {
-   
-    	System.out.println("Welcome TO Line Comparision Computation"); 			// Show prompting message
+    	System.out.println("Welcome TO Line Comparision Computation");			// Show prompting message
+    	//create LineComparision object using the LineComparision class
     	LineComparision lineComparision1 = new LineComparision();
     	System.out.println("Enter coordinates of line1");
-    	lineComparision1.enterCoordinates();  	
+    	lineComparision1.enterCoordinates();
+    	//create LineComparision object using the LineComparision class
     	LineComparision lineComparision2 = new LineComparision();
     	System.out.println("Enter coordinates of line2");
-    	lineComparision2.enterCoordinates();   	
+    	lineComparision2.enterCoordinates();
+    	
     	double length1 = lineComparision1.calculateLength();
     	System.out.println("Line1: "+length1);    	
     	double length2 = lineComparision2.calculateLength();
-    	System.out.println("Line2: "+length2);  	
-    	LineComparision lineComparision3 = new LineComparision();
-    	lineComparision3.compareTwoLengths(length1, length2);   	
-      }   
+    	System.out.println("Line2: "+length2);	
+    	
+    	compareTwoLengths(length1, length2);
+      }  
     private double calculateLength() {
+    	// computation
     	return Math.sqrt(Math.pow(x2 - x1,2) + Math.pow(y2 - y1,2));
     }
 }
